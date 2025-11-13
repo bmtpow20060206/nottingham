065084dc1847a4e75b095c0567e2f685a0aa857b
ilmafayaz <ilmaa.fayaz@gmail.com>
2025-11-13 15:54:14 +0800

    Update Mult.asm

--- CW1 - Files/Part_3/3.1_Assembly/Mult.asm
+++ CW1 - Files/Part_3/3.1_Assembly/Mult.asm
@@ -10,4 +10,44 @@
 
 // put your code here
 
+	@R3                 //result
+	M=0			// result=0
+	
+	@R1			//muliplicand
+	D=M
+	@R4
+	M=D			//R4=R1 copy multiplicand
 
+
+	@R2			//multiplier
+	D=M		
+	@R5
+	M=D			// R5=R2
+
+	@R5			//counter
+	D=M
+	@END
+	D;JEQ		//if counter==0,end
+
+(LOOP)
+	@R3
+	D=M
+	@R4
+	D=D+M
+	@R3
+	M=D			//R3=R3+R4
+	
+	@R5
+	D=M
+	D=D-1
+	@R5
+	M=D			//R5=R5-1
+
+	@R5
+	D = M
+	@LOOP
+	D;JGT		// if counter>0, continue loop
+
+(END)
+	@END
+	0;JMP	
